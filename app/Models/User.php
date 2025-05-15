b2fceba14ae28c0a533bbc922c1c11eef5e1d65e
Irawan Kilmer <irawankillmer@gmail.com>
2025-05-15 21:19:45 +0700

    Seeder user and role

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,19 +6,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles, HasUlids;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $role = Role::create(['name' => 'Admin']);
+
+        $user = User::create([
+            'username'  => 'admin1',
+            'email'     => 'admin1@gmail.com',
+            'password'  => Hash::make('admin1')
         ]);
+
+        $user->assignRole('Admin');
+
+        $rguru = Role::create(['name' => 'Guru']);
+
+        $guru = User::create([
+            'username'  => 'guru1',
+            'email'     => 'guru1@gmail.com',
+            'password'  => Hash::make('guru1')
+        ]);
+
+        $guru->assignRole('Guru');
+
+        $rsiswa = Role::create(['name' => 'Siswa']);
+
+        $siswa = User::create([
+            'username'  => 'siswa1',
+            'email'     => 'siswa1@gmail.com',
+            'password'  => Hash::make('siswa1')
+        ]);
+
+        $siswa->assignRole('Siswa');
     }
 }
